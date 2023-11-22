bcc6aada2e9ac8290ec1f1f9176ee7dd9c74c738
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-22 09:37:07 +0100

    - Do Exercise 2

--- src/main/java/org/pluppert/javatime/Main.java
+++ src/main/java/org/pluppert/javatime/Main.java
@@ -1,11 +1,20 @@
 package org.pluppert.javatime;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
         // Exercise 1:
+        /*
         LocalDate today = LocalDate.now();
         System.out.println("today = " + today);
+        */
+
+        // Exercise 2:
+        ZoneId zid = ZoneId.of("Europe/Stockholm");
+        String today = LocalDate.now(zid).format(DateTimeFormatter.ofPattern("eeee dd MMMM"));
+        System.out.println("today = " + (today.substring(0, 1).toUpperCase()) + today.substring(1));
     }
 }
