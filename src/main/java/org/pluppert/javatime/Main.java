dc1d5015dd81bfb73664792c85b557457428c8b0
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-22 08:31:15 +0100

    First commit: - Create project - Create packages 'javatime' and 'strings' . Create Main classes in new packages

---
+++ src/main/java/org/pluppert/javatime/Main.java
@@ -0,0 +1,7 @@
+package org.pluppert.javatime;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/pluppert/strings/Main.java
@@ -0,0 +1,7 @@
+package org.pluppert.strings;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
