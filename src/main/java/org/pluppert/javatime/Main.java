72e077f4cd11290f748e897f0a6648e13ec767a1
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-22 08:39:39 +0100

    - Do Exercise 1

--- src/main/java/org/pluppert/javatime/Main.java
+++ src/main/java/org/pluppert/javatime/Main.java
@@ -1,7 +1,11 @@
 package org.pluppert.javatime;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
+        // Exercise 1:
+        LocalDate today = LocalDate.now();
+        System.out.println("today = " + today);
     }
 }
