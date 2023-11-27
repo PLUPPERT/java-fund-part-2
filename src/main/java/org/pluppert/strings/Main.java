0b3b3aa98c44c21fdad289ca71f2db753de0f342
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-27 15:35:39 +0100

    - Do Exercise 5

--- src/main/java/org/pluppert/strings/Main.java
+++ src/main/java/org/pluppert/strings/Main.java
@@ -3,8 +3,15 @@ package org.pluppert.strings;
 public class Main {
     public static void main(String[] args) {
         // Exercise 3:
+        /*
         String sentence = "Ok this is not as long!";
         String subString = sentence.substring(11, sentence.length() - 1);
         System.out.println("subString = " + subString);
+        */
+
+        // Exercise 5:
+        String javaWorst = "Java is the worst programming language!";
+        String javaBest = javaWorst.replace("worst", "best");
+        System.out.println("javaBest = " + javaBest);
     }
 }
